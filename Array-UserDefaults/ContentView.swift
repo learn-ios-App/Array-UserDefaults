317aead997a735b9e308d5b19a6ff41f938546b4
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-11 13:42:49 +0900

    commit

--- Array-UserDefaults/ContentView.swift
+++ Array-UserDefaults/ContentView.swift
@@ -1,21 +1,33 @@
-//
-//  ContentView.swift
-//  Array-UserDefaults
-//
-//  Created by 渡邊魁優 on 2023/02/11.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(name)
+            TextField("名前を入力", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 150)
+            Button(action: {
+                setName(name: name)
+            }) {
+                Text("保存")
+            }
         }
-        .padding()
+        .onAppear() {
+            name = updateName()
+        }
+    }
+    func setName(name: String) {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(name, forKey: "nameKey")
+    }
+    
+    func updateName() -> String {
+        let userDefaults = UserDefaults.standard
+        let name = userDefaults.string(forKey: "nameKey") ?? ""
+        return name
     }
 }
 
