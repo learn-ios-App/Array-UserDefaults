df21bed6b3377a5da4d2ab5ef52ddd7dc6a94cf3
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-11 13:22:01 +0900

    Initial Commit

---
+++ Array-UserDefaults/Array_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Array_UserDefaultsApp.swift
+//  Array-UserDefaults
+//
+//  Created by 渡邊魁優 on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct Array_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
